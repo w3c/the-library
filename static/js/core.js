c34684cb9255ca0a3b2d5b8419bb0a433e3684ca
Robin Berjon <robin@berjon.com>
2013-02-26 15:05:39 +0100

    rely more on couth for the form bits

--- static/js/core.js
+++ static/js/core.js
@@ -17,22 +17,46 @@ angular.module("the-library", ["the-library-api"])
         var specs = Specs.list();
         $scope.specs = specs.rows;
         $scope.count = specs.total_rows;
-        // XXX don't do that
+    })
+    // XXX
+    // move this to a generic couth module
+    .controller("CouthFormCtrl", function ($scope) {
+        $scope.shortName = "test-me";
+        $scope.sources = [
+            { url: "http://berjon.com/specA", type: "respec-source" }
+        ,   { url: "http://www.org/TR/html5", type: "html-spec" }
+        ];
+        $scope.$couthSave = function () {
+            // check that it's valid, if not return
+            console.log($scope);
+        };
+        $scope.$couthArrayDel = function (path, idx, evt) {
+            $scope.$eval(path).splice(idx, 1);
+            evt.preventDefault();
+        };
+        $scope.$couthArrayAdd = function (path, type, evt) {
+            var empty;
+            if (type === "object" || type === "any" || !type) empty = {};
+            else if (type === "array") empty = [];
+            else if (type === "string") empty = "";
+            else if (type === "number") empty = 0;
+            else if (type === "boolean") empty = false;
+            else if (type === "null") empty = null;
+            console.log(path, $scope.$eval(path));
+            $scope.$eval(path).push(empty);
+            evt.preventDefault();
+        };
         // replace with a real controller on the form that:
-        //  - can do new
+        //  - can do new (how does one reset automatically? need to generate empty shallow instance from schema)
         //  - can do edit
         //  - can serialise and send, taking _id, _rev, and proper URL/$resource.action into account
-        // $type needs to be part of the form (both new and edit), remove that silly code
         // UI to show form (new button)
+        // UI for arrays
         // add a bunch of content
         // list content
         //  - click row to edit
         // pagination
-        var $formScope = angular.element($("#spec-form")[0]).scope();
-        $formScope.shortName = "test-me";
-        $formScope.sources = [
-            { url: "http://berjon.com/specA", type: "respec-source" }
-        ,   { url: "http://www.org/TR/html5", type: "html-spec" }
-        ];
+        // some common CSS complementing bootstrap loaded in /couth/css/form.css
+        
     })
 ;
